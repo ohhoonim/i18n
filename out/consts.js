f0e55e1ac5bd27a311db15ee58b72aa0eb4247c2
matthew <matthew@nexus.co.kr>
2021-06-04 18:18:35 +0900

    'init'

---
+++ out/consts.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    host: "10.10.30.12",
+    port: "3306",
+    user: "nexus",
+    pass: "nexus!234"
+};
+//# sourceMappingURL=consts.js.map
---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+export default {
+    host: "10.10.30.12",
+    port: "3306",
+    user: "nexus",
+    pass: "nexus!234"
+};
